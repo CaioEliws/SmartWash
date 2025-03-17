018556aa9e479cd7a9c546c42278659552cb167c
CaioEliws <caioeliasvieira2@gmail.com>
2025-03-17 12:49:43 -0300

    Implement login and registration screens

--- app/src/main/java/com/example/smartwash/MainActivity.kt
+++ app/src/main/java/com/example/smartwash/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartwash
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val registerText = findViewById<View>(R.id.registerText) // Pegue o TextView que mostra "Registrar-se"
+
+        registerText.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
